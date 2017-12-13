806560943e7d195ca4880e55618800e1d8f84a30
Mohammad Haveliwala <mohammmad.haveliwala@gmail.com>
2017-12-12 20:11:54 -0500

    model made

---
+++ ConstructModel.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+import os
+
+z = 2
+numberOfImages = 0
+for images in os.listdir('trainingImages') :
+    numberOfImages+=1
+im = cv2.imread('trainingImages/myFace0.png', cv2.IMREAD_GRAYSCALE)
+im2 = cv2.imread('trainingImages/myFace1.png', cv2.IMREAD_GRAYSCALE)
+im = im.flatten()
+im2 = im2.flatten()
+m = np.column_stack((im, im2))
+while z < numberOfImages :
+    img = cv2.imread('trainingImages/myFace' + str(z) + '.png', cv2.IMREAD_GRAYSCALE)
+    img = img.flatten()
+    m = np.concatenate((m, img[:, None]), axis=1)
+    z += 1
+np.save('trainingFacesMatrix.npy', m)
